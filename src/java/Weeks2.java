7db55a64825bd5b54bf168e527a630a167d18d5a
MOMO <MOMO@DESKTOP-U8VH0RR>
2023-03-21 16:27:18 +0900

    자바 프로그래밍 2주차 과제

--- src/java/Weeks2.java
+++ src/java/Weeks2.java
@@ -1,5 +1,29 @@
 package java;
-
+import java.util.Scanner;
 public class Weeks2 {
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      
+	      String sign;
+	      System.out.print("연산>>");
+	      double num = sc.nextDouble();
+	      sign = sc.next();
+	      double num1 = sc.nextDouble();
+	      
+	      if(sign.equals("+"))
+	         System.out.print(num+sign+num1+"의 계산 결과는 "+(num + num1));
+	      else if(sign.equals("-"))
+	          System.out.print(num+sign+num1+"의 계산 결과는 "+(num - num1));
+	      else if(sign.equals("*"))
+	         System.out.print(num+sign+num1+"의 계산 결과는 "+(num*num1));
+	      else if(sign.equals("/"))
+	      {  
+	         if(num1==0)
+	            System.out.print("0으로 나눌 수 없습니다.");
+	         else
+	            System.out.print(num+sign+num1+"의 계산 결과는 "+(num/num1));
+	      }
+	      sc.close();
+	   }
 
 }
--- src/java/Weeks2_2.java
+++ src/java/Weeks2_2.java
@@ -1,5 +1,41 @@
 package java;
-
+import java.util.Scanner;
 public class Weeks2_2 {
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      
+	      String sign;
+	      
+	      System.out.print("연산>> ");
+	      double num = sc.nextDouble();
+	      sign = sc.next();
+	      double num1 = sc.nextDouble();
+	 
+	      switch(sign) {
+	      case "+" :
+	         System.out.print(num+sign+num1+"의 계산 결과는 "+(num+num1));
+	         break;
+	      case "-":
+	          System.out.print(num+sign+num1+"의 계산 결과는 "+(num-num1));
+	         break;
+	      case "*":
+	          System.out.print(num+sign+num1+"의 계산 결과는 "+(num*num1));
+	         break;
+	      case "/":
+	         if(num1==0) {
+	            System.out.print("0으로 나눌 수 없습니다.");
+	            break;
+	         }
+	         else {
+	            System.out.print(num+sign+num1+"의 계산 결과는 "+(num/num1));
+	            break;
+	         }
+	      default:
+	         System.out.print("잘못 입력하였습니다.");
+	         break;
+	      }
+	      sc.close();
+	   }
+
 
 }
